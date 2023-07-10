0c92ff28155e47123ecc42897e26270f36eacc60
henriquecrono <henriquemiranda5@hotmail.com>
2023-07-10 16:56:23 -0300

    add page Home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<main className="min-h-screen-64-pixel flex flex-col items-center">
+			<h1>Home</h1>
+			<p>Welcome.</p>
+			<p>WIP.</p>
+		</main>
+	);
+}
