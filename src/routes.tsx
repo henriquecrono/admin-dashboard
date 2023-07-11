c994d5534753691ee8cdae489c8e6bd8eb6f1ff8
henriquecrono <henriquemiranda5@hotmail.com>
2023-07-11 13:39:21 -0300

    add route to pokemon list

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,11 +4,17 @@ import { AuthenticatedLayout } from './components/AuthenticatedLayout';
 
 import Home from './pages/home';
 
+import { PokemonList } from './pages/pokemon';
+
 export default function RoutesComponent() {
 	return (
 		<Routes>
 			<Route element={<AuthenticatedLayout />}>
 				<Route path="/" element={<Home />} />
+			</Route>
+
+			<Route element={<AuthenticatedLayout />}>
+				<Route path="/pokemon" element={<PokemonList />} />
 			</Route>
 		</Routes>
 	);
